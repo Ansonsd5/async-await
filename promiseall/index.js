7e865020c413552272a286e334c54d5976041785
Ansonsd5 <anson11sd@gmail.com>
2024-02-22 20:47:01 +0530

    promise all and this key word

--- promiseall/index.js
+++ promiseall/index.js
@@ -1,22 +1,35 @@
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Success after 2sec for p1");
-  }, 2000);
-});
+const promise1 = new Promise((resolve) =>
+  setTimeout(() => resolve("Promise 1 resolved"), 1000)
+);
+const promise2 = new Promise((resolve) =>
+  setTimeout(() => resolve("Promise 2 resolved"), 2000)
+);
 
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("promise two success afetr 3000sec");
-  }, 3000);
-});
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(() => reject("Promise 3 rejected"), 1500)
+);
 
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("promise 3 successs afetr a sec");
-  }, 1000);
-});
+const promise4 = new Promise((resolve) =>
+  setTimeout(() => resolve("Promise 4 resolved"), 1500)
+);
 
 
-const allResponses = Promise.all([promise1,promise2,promise3]);
+// we will call Promise.all([promise1, promise2, promise3]) with promise1,promise2, promise3.Since promise3 will get rejects in 1500ms,  If any of the input Promises are rejected immediately rejects with the reason of the first Promise that was rejected. It doesn't wait for the rest of the Promises to complete.
+ 
+Promise.all([promise1, promise2, promise3])
+  .then((values) => {
+    console.log("All promises resolved:", values);
+  })
+  .catch((error) => {
+    console.error("One of the promises rejected:", error);
+  });
 
-allResponses.then((res)=> console.log(res))
+
+  //This will return when all promises will resovle.
+  Promise.all([promise1, promise2, promise4])
+  .then((values) => {
+    console.log("All promises resolved:", values);
+  })
+  .catch((error) => {
+    console.error("One of the promises rejected:", error);
+  });
---
+++ thiskeyword/index.js
@@ -0,0 +1,81 @@
+//this key word differ its behavior for differnt run  environments
+//eg:
+//this in browser reffernce to window object i.e this === window
+// module.exports in Node.js and window in a browser environment both serve as the global scope objects, but they are specific to their respective environments and have different functionalities and purposes.
+"use strict";
+
+console.log(this); //window
+
+function x() {
+  console.log(this);
+}
+
+//The value of this key words inside function depends on the strict or non strict mode
+
+//if we are in strict mode this inside function will be undefined
+
+//If we nit using strict mode then the value of this which is null or undefined then this substitution will happen and this will substitute to window obj
+
+//how we call also depends
+x(); //undefined
+
+window.x(); //window
+
+let student = {
+  firstName: "Anson",
+  lastName: "Dsouza",
+  printFullName: function () {
+    console.log(`My name is ${this.firstName} last name is ${this.lastName}`);
+  },
+};
+
+let student2 = {
+  firstName: "Robert",
+  lastName: "Silva",
+  x: function () {
+    console.log(this);
+  },
+};
+
+student.printFullName();
+
+student2.x();
+
+student.printFullName.call(student2); // use of call
+
+//This will behave diffenetly in normal function and the arrow functions
+
+const obj = {
+  num1: 6,
+  num2: 8,
+  add: function () {
+    console.log(this.num1);
+    console.log(this) 
+ 
+  },
+};
+
+obj.add();
+
+const obj2 = {
+  num1: 4,
+  num2: 10,
+  x: function () {
+    const add = () => {
+        // value of the enclosing lexical context here its obj2
+      let sum = this.num1 + this.num2;
+      console.log(sum);
+    };
+    add();
+  },
+};
+
+obj2.x();
+
+
+let arrowObj ={
+    arrow : () =>{
+        console.log(this);
+    }
+}
+arrowObj.arrow()
